458f131a2ab76b26b1a9dc11143e78840d1894b4
Maria Eduarda <silvamariaeduarda92@gmail.com>
2024-02-11 16:04:59 -0300

    Projeto terminado de acordo com o vídeo

--- js/script.js
+++ js/script.js
@@ -10,12 +10,24 @@ const jump = () => {
 }
 
 const loop = setInterval(() => {
+
+    console.log('loop');
+
     const pipePosition = pipe.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', ' ');
 
     if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80){
         pipe.style.animation = 'none';
         pipe.style.left = '${pipePosition}px';
+
+        pipe.style.animation = 'none';
+        pipe.style.bottom = '${marioPosition}px';
+
+        mario.src = './img/game-over.png';
+        mario.style.width = '75px';
+        mario.style.marginLeft = '50px';
+
+        clearInterval(loop);
     }
 }, 10);
 
